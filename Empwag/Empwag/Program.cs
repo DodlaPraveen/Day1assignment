0880881d8c33ab614c36fcf68deb548c3b7b3055
praveen dodla <dodlapraveen09@gmail.com>
2022-07-25 21:26:42 +0530

    implemented uc1

---
+++ Empwag/Empwag/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Empwag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            if (name == "praveen")
+            {
+                Console.WriteLine("Name is ok");
+            }
+            else
+            {
+                Console.WriteLine("Name is Not ok");
+            }
+        }
+    }
+}
